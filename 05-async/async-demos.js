19047dc8c5e249f3671c7548ea2b1e9d4bccb8f7
Magesh Kuppan <tkmagesh77@gmail.com>
2019-08-16 12:20:22 +0800

    async using callback

---
+++ 05-async/async-demos.js
@@ -0,0 +1,35 @@
+(function(){
+
+	function addSync(x,y){
+		console.log(`	[@Service] processing ${x} and ${y}`);
+		var result = x + y;
+		console.log(`	[@Service] returning result`);
+		return result;
+	}
+
+	function addSyncClient(x,y){
+		console.log(`[@Client] triggering the service`);
+		var result = addSync(x,y);
+		console.log(`[@Client] result = ${result}`);
+	}
+
+	window['addSyncClient'] = addSyncClient;
+
+	function addAsync(x,y, callback){
+		console.log(`	[@Service] processing ${x} and ${y}`);
+		setTimeout(function(){
+			var result = x + y;
+			console.log(`	[@Service] returning result`);
+			callback(result);
+		},4000);
+	}
+
+	function addAsyncClient(x,y){
+		console.log(`[@Client] triggering the service`);
+		addAsync(x,y, function(result){
+			console.log(`[@Client] result = ${result}`);
+		});
+	}
+
+	window['addAsyncClient'] = addAsyncClient;
+})();
